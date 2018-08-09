1cf7ada3d2fc7e1a23019b41d24102f6039d90b0
David Darke <david@atomicsmash.co.uk>
2018-08-09 13:46:37 +0100

    Refactored into a class. So classy 💅

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # WordPress - Process External Links
+
+
+This plugin adds `target="_blank" rel="nofollow noopener"` properties to any **external** link added through the WYSIWYG editor.
--- wp-process-external.php
+++ wp-process-external.php
@@ -8,54 +8,67 @@ Version: 0.0.1
 Author URI: http://atomicsmash.co.uk
 */
 
-// Regex find anchor tags based on https://regexr.com/3tjfs
-// Then pass results to adjust_link
-function wp_pel_find_anchor_tags($content)
+class ProcessExternalLinks
 {
 
-    $pattern ="/<a(.*?)href=\"(.*?)\"(.*?)>/i";
+    function __construct(){
 
-    $content = preg_replace_callback( $pattern, 'wp_pel_adjust_link', $content );
-
-    return $content;
-
-}
-
-// Process links to work out if links are internal or external, then adjust accordingly
-function wp_pel_adjust_link( $link_parts )
-{
-
-    global $wp_query,$post;
-
-    // echo "<pre>";
-    // print_r($post->post_type);
-    // echo "</pre>";
-    // die();
-
-    if ( is_array( $link_parts )) {
-        $site_url = home_url();
-        $link_type = 'external';
-
-        $found = false;
-
-        $pos = strpos( $link_parts[2], $site_url );
-
-        // if the site_url is not found in the link URL, modify the '<a' link with external parameters
-        if ($pos === false) {
-            return "<strong>EXTERNAL LINK FOUND</strong><a". $link_parts[1] ."href='".$link_parts[2]."'".$link_parts[3]." rel='no-follow'>";
-        }else{
-            return "<strong>INTERNAL LINK FOUND</strong><a". $link_parts[1] ."href='".$link_parts[2]."'".$link_parts[3]." rel='no-follow'>";
-
-            // Return default link
-            // return $link_parts[0];
-
-        }
+        add_filter('the_content', array( $this, 'wp_pel_find_anchor_tags' ), 11);
 
     }
 
-    return $link_parts[0];
+    // Regex find anchor tags based on https://regexr.com/3tjfs
+    // Then pass results to adjust_link
+    function wp_pel_find_anchor_tags($content)
+    {
+
+        $pattern ="/<a(.*?)href=\"(.*?)\"(.*?)>/i";
+
+        $content = preg_replace_callback( $pattern, array( $this, 'wp_pel_adjust_link'), $content );
+
+        return $content;
+
+    }
+
+    // Process links to work out if links are internal or external, then adjust accordingly
+    function wp_pel_adjust_link( $link_parts )
+    {
+
+        global $wp_query,$post;
+
+        if ( is_array( $link_parts )) {
+            $site_url = home_url();
+            $link_type = 'external';
+
+            $found = false;
+
+            $pos = strpos( $link_parts[2], $site_url );
+
+            // if the site_url is not found in the link URL, modify the '<a' link with external parameters
+            if ($pos === false) {
+
+                $link_parts[3] = ' target="_blank" rel="nofollow noopener" ';
+
+                return "<strong>EXTERNAL LINK FOUND</strong><a". $link_parts[1] ."href='".$link_parts[2]."'".$link_parts[3].">";
+
+            }else{
+                return "<strong>INTERNAL LINK FOUND</strong><a". $link_parts[1] ."href='".$link_parts[2]."'".$link_parts[3].">";
+
+                // Return default link
+                // return $link_parts[0];
+
+            }
+
+        }
+
+        return $link_parts[0];
+
+    }
 
 }
 
 
-add_filter('the_content', 'wp_pel_find_anchor_tags',11);
+
+
+
+new ProcessExternalLinks;
